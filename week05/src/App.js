0cddd2b83b03453d93ad58c3b48092047f2c9c06
00kang <kdb070970@gmail.com>
2023-10-26 21:29:12 +0900

    feat: 다양한 페이지 컴포넌트 만들고 리액트 라우팅 설정하기

--- week05/src/App.js
+++ week05/src/App.js
@@ -1,37 +1,23 @@
-import { movies } from "./movieDummy.js";
-import Movie from "./Components/Movie.jsx";
-import styled from "styled-components";
-import { createGlobalStyle } from "styled-components";
-
-export const GlobalStyle = createGlobalStyle`
-  body {
-    background-color: #22254b;
-    font-family: sans-serif;
-  }
-`;
-export const AppContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-`;
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Celebrity from './Components/Celebrity.jsx';
+import Home from './Components/Home.jsx';
+import Movies from './Components/Movies.jsx';
+import NotFound from './Components/NotFound.jsx';
+import TV from './Components/TV.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <GlobalStyle />
-      <AppContainer>
-        {movies.results.map((item) => {
-          return (
-            <Movie
-              key={item.id}
-              poster_path={item.poster_path}
-              title={item.title}
-              vote_average={item.vote_average}
-              overview={item.overview}
-            />
-          );
-        })}
-      </AppContainer>
+    <div className="root-wrap">
+      <BrowserRouter>
+        {/* 라우팅에 영향을 받지 않는 header, Footer 등의 컴포넌트의 위치 */}
+        <Routes>
+          <Route path="/" element={Home} />
+          <Route path="/Celebrity" element={Celebrity} />
+          <Route path="/Movies" element={Movies} />
+          <Route path="/NotFound" element={NotFound} />
+          <Route path="/TV" element={TV} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
