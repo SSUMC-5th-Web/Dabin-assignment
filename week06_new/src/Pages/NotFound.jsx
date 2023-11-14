8557de38b93899334209914f7a512899e6712614
00kang <kdb070970@gmail.com>
2023-11-14 16:54:04 +0900

    feat: useNavigate Hook 활용하여 NotFound 컴포넌트 구현 완료

--- week06_new/src/Pages/NotFound.jsx
+++ week06_new/src/Pages/NotFound.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 50px 100px;
+`;
+export const P = styled.p`
+  font-size: ${props => props.size || '16px'};
+  font-weight: ${props => props.weight || 'normal'};
+  color: ${props => props.color || 'black'};
+`;
 
 function NotFound() {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+  const onClickHome = () => {
+    navigate(`/`);
+  };
+
+  return (
+    <Container>
+      <P size="24px" weight="bold">
+        해당 페이지를 찾지 못했습니다.
+      </P>
+      <P>주소가 잘못되었거나 더 이상 제공되지 않는 페이지입니다.</P>
+      <P onClick={onClickHome} style={{ cursor: 'pointer' }} color="red">
+        메인 페이지로 이동
+      </P>
+    </Container>
+  );
 }
 
 export default NotFound;
