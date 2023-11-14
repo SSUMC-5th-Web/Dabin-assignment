068e569c911ba0937d23ff3bfc71dc78ea9f2d90
00kang <kdb070970@gmail.com>
2023-11-14 17:30:15 +0900

    feat: MovieDetail 컴포넌트 구현 및 스타일 지정

--- week06_new/src/Pages/MovieDetail.jsx
+++ week06_new/src/Pages/MovieDetail.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import { useLocation, useParams } from 'react-router-dom';
+import styled from 'styled-components';
 
+export const Container = styled.div`
+  padding: 50px 100px;
+  display: flex;
+  gap: 50px;
+`;
+export const P = styled.p`
+  font-size: ${props => props.size || '16px'};
+  font-weight: ${props => props.weight || 'normal'};
+  color: ${props => props.color || 'black'};
+`;
+export const Img = styled.img`
+  height: 400px;
+`;
 function MovieDetail() {
   const { title } = useParams();
   const { state } = useLocation();
 
   return (
-    <div>
-      <h2>{title}</h2>
-      <img src={state.posterPath} alt={title} />
-      <p>{state.overview}</p>
-    </div>
+    <Container>
+      <Img src={state.posterPath} alt={title} />
+      <div>
+        <P size="24px" weight="bold" style={{ marginTop: '0' }}>
+          {title}
+        </P>
+        <P>{`${state.voteAverage} / 10점`}</P>
+        <P>{state.overview}</P>
+      </div>
+    </Container>
   );
 }
 
