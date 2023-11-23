a8e78dab0c7cbdd6b563852455e2387fdb8b2061
00kang <kdb070970@gmail.com>
2023-11-24 03:47:54 +0900

    feat(TV.jsx): tvDummy.js 의 데이터를 이용해서 TV 리스트 보여주기 - 기존 컴포넌트 속성 이용해서 만들기

--- week07/src/Pages/TV.jsx
+++ week07/src/Pages/TV.jsx
@@ -1,53 +1,33 @@
 import React from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
+import * as A from './Movies.style.jsx';
 
-const GlobalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle`
   body {
     background-color: #22254b;
     font-family: sans-serif;
   }
 `;
-const AppContainer = styled.div`
+export const AppContainer = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: flex-start;
-  padding: 40px 100px;
+  padding: 0 100px;
   width: 100%;
 `;
 
-const TvContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  background-color: white;
-  padding: 5px;
-  margin: 5px;
-  width: 310px;
-`;
-const TvImg = styled.img`
-  height: auto;
-  width: 300px;
-  display: block;
-  margin: 0 auto;
-`;
-const TvContents = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 5px;
-`;
-
-function TvItem({ item }) {
+function TVItem({ item }) {
   const base_url = 'https://image.tmdb.org/t/p/w1280/';
+
   return (
-    <TvContainer>
-      <TvImg src={base_url + item.backdrop_path} alt={item.name} />
-      <TvContents>
-        <span style={{ fontWeight: 'bold' }}> 제목 : {item.name}</span>
-        <span> 첫 방영일 : {item.first_air_date}</span>
-        <span> 평점 : {item.vote_average} / 10</span>
-      </TvContents>
-    </TvContainer>
+    <A.movieContainer style={{ width: '300px' }}>
+      <A.movieImg src={base_url + item.backdrop_path} alt={item.name} />
+      <A.movieInfo>
+        <h4 style={{ margin: '0' }}>{item.name}</h4>
+        <span style={{ marginLeft: '5px' }}>{item.vote_average}</span>
+      </A.movieInfo>
+    </A.movieContainer>
   );
 }
 
@@ -57,11 +37,10 @@ function TV({ tvs }) {
       <GlobalStyle />
       <AppContainer>
         {tvs.map(item => (
-          <TvItem item={item} key={item.title} />
+          <TVItem item={item} key={item.name} /> // 각 TV 프로그램 항목을 TVItem 컴포넌트로 렌더링
         ))}
       </AppContainer>
     </div>
   );
 }
-
 export default TV;
