34d4d2776d30f781f9458fdea83dba5df3c5a480
00kang <kdb070970@gmail.com>
2023-11-24 00:02:57 +0900

    feat(Login.jsx): 유효성 검사 추가

--- week07/src/Pages/Login.jsx
+++ week07/src/Pages/Login.jsx
@@ -6,15 +6,31 @@ function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
   const navigate = useNavigate();
 
   const handleEmailChange = e => {
     setEmail(e.target.value);
-    setEmailError('');
   };
 
   const handlePasswordChange = e => {
     setPassword(e.target.value);
+  };
+
+  const handleEmailBlur = () => {
+    if (!email.includes('@')) {
+      setEmailError('올바른 이메일을 입력해주세요.');
+    } else {
+      setEmailError('');
+    }
+  };
+
+  const handlePasswordBlur = () => {
+    if (!/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(password)) {
+      setPasswordError('영문, 숫자, 특수문자를 포함하여 8자 이상 입력해주세요.');
+    } else {
+      setPasswordError('');
+    }
   };
 
   const handleSubmit = e => {
@@ -22,6 +38,8 @@ function Login() {
 
     if (!email.includes('@')) {
       setEmailError('올바른 이메일을 입력해주세요.');
+    } else if (!/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(password)) {
+      setPasswordError('영문, 숫자, 특수문자를 포함하여 8자 이상 입력해주세요.');
     } else {
       navigate('/');
     }
@@ -36,7 +54,7 @@ function Login() {
       <FormContainer onSubmit={handleSubmit}>
         <Label>
           이메일 주소
-          <Input type="text" value={email} onChange={handleEmailChange} />
+          <Input type="text" value={email} onChange={handleEmailChange} onBlur={handleEmailBlur} />
           {emailError && <WarningMessage>{emailError}</WarningMessage>}
         </Label>
         <Label>
@@ -45,10 +63,14 @@ function Login() {
             type="password"
             value={password}
             onChange={handlePasswordChange}
+            onBlur={handlePasswordBlur}
             placeholder="영문, 숫자, 특수문자 포함 8자 이상"
           />
+          {passwordError && <WarningMessage>{passwordError}</WarningMessage>}
         </Label>
-        <Button type="submit">확인</Button>
+        <Button type="submit" disabled={!email.includes('@') || password.length < 8}>
+          확인
+        </Button>
       </FormContainer>
     </Container>
   );
