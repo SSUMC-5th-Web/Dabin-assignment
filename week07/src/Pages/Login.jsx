f4aeb8bd08e09f8a00bef2cb859e18d0d9f79281
00kang <kdb070970@gmail.com>
2023-11-24 03:38:48 +0900

    refactor(Login.jsx): useState 개수를 줄여서 관리해야 할 데이터 줄이기

--- week07/src/Pages/Login.jsx
+++ week07/src/Pages/Login.jsx
@@ -3,44 +3,28 @@ import { useNavigate } from 'react-router-dom';
 import { Container, FormContainer, Title, Label, Input, WarningMessage, Button } from './Login.style';
 
 function Login({ onLogin }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
+  const [form, setForm] = useState({ email: '', password: '' });
+  const [errors, setErrors] = useState({ email: '', password: '' });
   const navigate = useNavigate();
 
-  const handleEmailChange = e => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = e => {
-    setPassword(e.target.value);
-  };
-
-  const handleEmailBlur = () => {
-    if (!email.includes('@')) {
-      setEmailError('올바른 이메일을 입력해주세요.');
-    } else {
-      setEmailError('');
-    }
-  };
-
-  const handlePasswordBlur = () => {
-    if (!/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(password)) {
-      setPasswordError('영문, 숫자, 특수문자를 포함하여 8자 이상 입력해주세요.');
-    } else {
-      setPasswordError('');
-    }
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setForm(prev => ({ ...prev, [name]: value }));
   };
 
   const handleSubmit = e => {
     e.preventDefault();
 
-    if (!email.includes('@')) {
-      setEmailError('올바른 이메일을 입력해주세요.');
-    } else if (!/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(password)) {
-      setPasswordError('영문, 숫자, 특수문자를 포함하여 8자 이상 입력해주세요.');
-    } else {
+    const newErrors = {
+      email: !form.email.includes('@') ? '올바른 이메일을 입력해주세요.' : '',
+      password: !/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(form.password)
+        ? '영문, 숫자, 특수문자를 포함하여 8자 이상 입력해주세요.'
+        : '',
+    };
+
+    setErrors(newErrors);
+
+    if (!newErrors.email && !newErrors.password) {
       onLogin();
       navigate('/');
     }
@@ -55,21 +39,21 @@ function Login({ onLogin }) {
       <FormContainer onSubmit={handleSubmit}>
         <Label>
           이메일 주소
-          <Input type="text" value={email} onChange={handleEmailChange} onBlur={handleEmailBlur} />
-          {emailError && <WarningMessage>{emailError}</WarningMessage>}
+          <Input type="text" name="email" value={form.email} onChange={handleChange} />
+          {errors.email && <WarningMessage>{errors.email}</WarningMessage>}
         </Label>
         <Label>
           비밀번호
           <Input
             type="password"
-            value={password}
-            onChange={handlePasswordChange}
-            onBlur={handlePasswordBlur}
+            name="password"
+            value={form.password}
+            onChange={handleChange}
             placeholder="영문, 숫자, 특수문자 포함 8자 이상"
           />
-          {passwordError && <WarningMessage>{passwordError}</WarningMessage>}
+          {errors.password && <WarningMessage>{errors.password}</WarningMessage>}
         </Label>
-        <Button type="submit" disabled={emailError || passwordError}>
+        <Button type="submit" disabled={errors.email || errors.password}>
           확인
         </Button>
       </FormContainer>
