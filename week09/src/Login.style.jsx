219e79f3ced05af08fbba7703feac6b45fcd52de
00kang <kdb070970@gmail.com>
2023-11-26 14:33:09 +0900

    feat(Login.jsx): 서버 포트와 axios로서 데이터를 주고 받는 로그인 페이지 구현

---
+++ week09/src/Login.style.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 150px;
+  height: 100vh;
+  margin: 0 auto;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 20px;
+  border-radius: 5px;
+  border: 1px solid black;
+  padding: 5px;
+  margin-bottom: 10px;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  height: 30px;
+  border-radius: 5px;
+  border: 1px solid black;
+`;
