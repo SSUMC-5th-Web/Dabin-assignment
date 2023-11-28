16a3a48363773de6ee5f3ffcf85387ebfdef65cf
00kang <kdb070970@gmail.com>
2023-11-28 09:08:58 +0900

    feat(Login.jsx): 로컬스토리지에 토큰값이 저장되어 있다면 username을 활용한 문구 출력

--- week09/src/Login.jsx
+++ week09/src/Login.jsx
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
-import { Container, Input, Button } from "./Login.style";
+import React, { useState, useEffect } from "react";
+import { Container, Input, Button, LoadingContainer, HeaderContainer } from "./Login.style";
 import axios from "axios";
 
 function Login() {
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
+  const [token, setToken] = useState(null);
+  const [username, setUsername] = useState("");
   const [loading, setLoading] = useState(false);
+
+  // 컴포넌트가 마운트 될 때 로컬스토리지를 확인하여 토큰과 username을 상태로 설정
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    const storedUsername = localStorage.getItem("username");
+
+    if (storedToken) {
+      setToken(storedToken);
+    }
+
+    if (storedUsername) {
+      setUsername(storedUsername);
+    }
+  }, []);
 
   const handleLogin = () => {
     if (id === "" || password === "") {
@@ -18,27 +34,55 @@ function Login() {
     axios
       .post("/user/login", { id, pw: password })
       .then((response) => {
-        const { message, result } = response.data;
-        const { AccessToken, userId, username } = result;
-        console.log(`${message}, userId: ${userId}, username: ${username}`);
-
-        // 로그인 성공 시 토큰과 id를 로컬스토리지에 저장
-        localStorage.setItem("token", AccessToken);
-        localStorage.setItem("id", userId);
-      })
-      .catch((error) => {
-        // 404 Error : 아이디 에러
-        // 400 Error : 비밀번호 에러
-        console.log("에러:", error.response.data.message);
-      })
-      .finally(() => {
         setTimeout(() => {
+          const { AccessToken, userId, username } = response.data.result;
+
+          console.log(`Message: ${response.data.message}, userId: ${userId}, username: ${username}`);
+
+          // 로그인 성공 시 토큰과 id, username을 로컬스토리지에 저장
+          localStorage.setItem("token", AccessToken);
+          localStorage.setItem("id", userId);
+          localStorage.setItem("username", username);
+
+          // 토큰과 username을 상태에도 저장
+          setToken(AccessToken);
+          setUsername(username);
+
           setLoading(false);
         }, 1500);
+      })
+      .catch((error) => {
+        console.log("에러:", error.response.data.message);
+        setLoading(false);
       });
+    // .then((response) => {
+    //   const { AccessToken, userId, username } = response.data.result;
+
+    //   console.log(`Message: ${response.data.message}, userId: ${userId}, username: ${username}`);
+
+    //   // 로그인 성공 시 토큰과 id, username을 로컬스토리지에 저장
+    //   localStorage.setItem("token", AccessToken);
+    //   localStorage.setItem("id", userId);
+    //   localStorage.setItem("username", username);
+
+    //   // 토큰과 username을 상태에도 저장
+    //   setToken(AccessToken);
+    //   setUsername(username);
+    // })
+    // .catch((error) => {
+    //   console.log("에러:", error.response.data.message);
+    // })
+    // .finally(() => {
+    //   setTimeout(() => {
+    //     setLoading(false);
+    //   }, 1500);
+    // });
   };
 
-  return (
+  // 토큰이 있는 경우 로그인 정보를 보여줌, 그렇지 않은 경우 로그인 폼을 보여줌
+  return token ? (
+    <HeaderContainer>{username}님 안녕하세요</HeaderContainer>
+  ) : (
     <Container>
       <Input type="text" placeholder="아이디" value={id} onChange={(e) => setId(e.target.value)} />
       <Input type="password" placeholder="비밀번호" value={password} onChange={(e) => setPassword(e.target.value)} />
@@ -46,21 +90,9 @@ function Login() {
         로그인
       </Button>
       {loading && (
-        <div
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            backgroundColor: "rgba(0,0,0,0.5)",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
+        <LoadingContainer>
           <p style={{ color: "white" }}>Loading...</p>
-        </div>
+        </LoadingContainer>
       )}
     </Container>
   );
--- week09/src/Login.style.jsx
+++ week09/src/Login.style.jsx
@@ -25,3 +25,27 @@ export const Button = styled.button`
   border-radius: 5px;
   border: 1px solid black;
 `;
+
+export const LoadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 80px;
+  margin: auto auto;
+  background-color: beige;
+  font-size: 20px;
+  font-weight: 600;
+`;
